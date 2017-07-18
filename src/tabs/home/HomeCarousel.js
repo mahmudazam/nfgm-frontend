6168c082c339060427d6298bb38c8341e244ab95
Mahmud Azam (mfa256) <mfa256@mail.usask.ca>
2017-07-17 19:02:37 -0600

    HomeCarousel.js: Atom auto-indentation and styling of Carousel images; .gitignore: Ignoring assets directory

--- src/tabs/home/HomeCarousel.js
+++ src/tabs/home/HomeCarousel.js
@@ -1,6 +1,6 @@
 
 /*
-	Imports :
+Imports :
 */
 
 // React.js:
@@ -16,53 +16,53 @@ import Well from 'react-bootstrap/lib/Well'
 import fire from '../../fire'
 
 /**
- * Models a Carousel capable of pulling images from Firebase storage:
- */
+* Models a Carousel capable of pulling images from Firebase storage:
+*/
 class HomeCarousel extends React.Component {
-	/**
-	 * Initializes the list of image URLs to be used for rendering:
-	 */
-    constructor(props) {
-        super(props);
-        this.state = {
-        	imgURLs: []
-        };
-    }
-    
-    /**
-     * Loads the URL array with URLs stored in Firebase database:
-     */
-    componentWillMount(){
-    	// Get the reference to the database index of the image folder:
-        let dirRef = fire.database().ref('assets/img').orderByKey().limitToLast(100);
-        // Add every URL available in the index:
-        dirRef.on('child_added', snapshot => {
-            let image = { URL : snapshot.val().URL, id : snapshot.key }
-            this.setState({ imgURLs: this.state.imgURLs.concat([image]) });
-        })
-    }
+  /**
+  * Initializes the list of image URLs to be used for rendering:
+  */
+  constructor(props) {
+    super(props);
+    this.state = {
+      imgURLs: []
+    };
+  }
 
-	/**
-	 * Renders the Carousel:
-	 */
-    render() {
-        return(
-            <div className="col-lg-12 carouselWrapper">
-                <Well>NEWS:</Well>
-                <Carousel className="myCarousel">
-					{
-						// Map the array of URLs to Carousel items with img tags
-						// containing the URLs as src:
-						this.state.imgURLs.map((image) =>
-							<Carousel.Item key={image.id}>
-								<img width={500} height={300} alt="500x300" src={image.URL} />
-							</Carousel.Item>
-						)
-					}
-                </Carousel>
-            </div>
-        )
+  /**
+  * Loads the URL array with URLs stored in Firebase database:
+  */
+  componentWillMount(){
+    // Get the reference to the database index of the image folder:
+    let dirRef = fire.database().ref('assets/img').orderByKey().limitToLast(100);
+    // Add every URL available in the index:
+    dirRef.on('child_added', snapshot => {
+      let image = { URL : snapshot.val().URL, id : snapshot.key }
+      this.setState({ imgURLs: this.state.imgURLs.concat([image]) });
+    })
+  }
+
+  /**
+  * Renders the Carousel:
+  */
+  render() {
+    return(
+      <div className="col-lg-12 carouselWrapper">
+      <Well>NEWS:</Well>
+      <Carousel className="myCarousel">
+      {
+        // Map the array of URLs to Carousel items with img tags
+        // containing the URLs as src:
+        this.state.imgURLs.map((image) =>
+        <Carousel.Item key={image.id}>
+          <img className="carousel-img" alt="500x300" src={image.URL} />
+        </Carousel.Item>
+      )
     }
+    </Carousel>
+    </div>
+  )
+}
 };
 
 export default HomeCarousel;
