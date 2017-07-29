fa68153a417244cd7bef2be6871f9ff5266fc1c4
tayabsoomro <isoomro.t@gmail.com>
2017-07-29 14:50:43 -0600

    add icons

--- src/TopNavbar.js
+++ src/TopNavbar.js
@@ -1,11 +1,15 @@
 
 import React from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 
 class TopNavbar extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      phone_number: "(306) 764-5467"
+    }
   }
 
   render() {
@@ -16,16 +20,9 @@ class TopNavbar extends React.Component {
             <a href="#" className="navbar-brand" >Natural Fresh Grocery & Meat</a>
           </Navbar.Brand>
         </Navbar.Header>
-        <Nav className="pull-right" >
-          <NavItem eventKey={1} href="#">Link</NavItem>
-          <NavItem eventKey={2} href="#">Link</NavItem>
-          <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-            <MenuItem eventKey={3.1}>Action</MenuItem>
-            <MenuItem eventKey={3.2}>Another action</MenuItem>
-            <MenuItem eventKey={3.3}>Something else here</MenuItem>
-            <MenuItem divider />
-            <MenuItem eventKey={3.4}>Separated link</MenuItem>
-          </NavDropdown>
+        <Nav className="pull-right">
+          <NavItem eventKey={1} href="#"><Glyphicon glyph="earphone"/> { this.state.phone_number }</NavItem>
+
         </Nav>
       </Navbar>
 
