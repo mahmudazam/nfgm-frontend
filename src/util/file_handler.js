3f214933baf8bc49864a605e483f548cd31f4075
Mahmud Azam (mfa256) <mfa256@mail.usask.ca>
2017-08-23 21:32:00 -0600

    Implement pushing an item with all information

--- src/util/file_handler.js
+++ src/util/file_handler.js
@@ -25,23 +25,21 @@ const bucket = gcs.bucket(bucketName);
  * @param {String} readPath - path to the file to push
  * @param {String} refPath - the Firebase storage location to store the file
  * @param {String} storage_url - the URL of the Firebase storage location
+ * @param {Object} assetInfo - additional information about the asset
 */
-function pushAssetIndex(readPath , refPath, storage_url) {
+function pushAssetInfo(keyName, refPath, assetInfo) {
 	// Create path names:
-	readPath = path.normalize(readPath);
-	filename = path.basename(readPath);
 	if(refPath !== "") {
 		refPath = path.normalize(refPath);
 	}
 
 	// Create database node for file :
 	dbPath = path.normalize('assets/' + refPath + '/'
-			+ filename.replace('.' , '_'));
+			+ assetInfo[keyName].replace('.' , '_'));
 	dbRef = fire.database().ref(dbPath);
-	dbRef.child('Name').set(filename);
-	dbRef.child('URL').set(storage_url);
-	dbRef.child('Upload Time').set(new Date().toString());
-
+	Object.keys(assetInfo).map((key) => {
+		dbRef.child(key).set(assetInfo[key]);
+	});
 }
 
 /**
@@ -58,8 +56,9 @@ function createPublicFileURL(storageName) {
  * Pushes a file to Firebase storage with a corresponding database index:
  * @param {String} readPath - path to the file to push
  * @param {String} refPath - the Firebase storage location to store the file
+ * @param {Object} assetInfo - additional information about the asset
 */
-function pushAsset(readPath , refPath) {
+function pushAsset(readPath, refPath, keyName, assetInfo) {
 	// Upload to the bucket:
 	bucket.upload(readPath,
 		{ destination: refPath + '/' + path.basename(readPath),
@@ -77,9 +76,12 @@ function pushAsset(readPath , refPath) {
 			if(!URL) {
 				// We leave it blank:
 				URL = '';
+				return;
 			}
 			// An index for the file is added to the database:
-			pushAssetIndex(readPath , refPath , URL);
+			infoWithURL = JSON.parse(JSON.stringify(assetInfo));
+			infoWithURL.storage_urls = URL;
+			pushAssetInfo(keyName, refPath , infoWithURL);
 		}
 	);
 }
@@ -87,7 +89,12 @@ function pushAsset(readPath , refPath) {
 // Entry point for testing :
 
 // Pushing images to database:
-pushAsset("../assets/img/img1.jpg", "/img");
-pushAsset("../assets/img/img2.jpg", "/img");
-pushAsset("../assets/img/img3.jpg", "/img");
-pushAsset("../assets/img/img4.jpg", "/img");
+pushAsset('../../assets/img/exim-food-item-img/Beef/Beef 1.jpg', '/items',
+	'Item_Name',
+	{
+		Item_Name: 'Beef',
+		Price: 5.99,
+		Unit: 'kg',
+		Description: 'Fresh beef from the farms',
+		Sale: ''
+	});
